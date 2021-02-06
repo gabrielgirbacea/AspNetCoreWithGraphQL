dce4a2fc3c855531c51fc6d231017c519279a664
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-06 14:15:03 +0200

    Created entities, dbcontext and configured the Startup.cs to use the dbcontext.
    Created entities, dbcontext and configured the Startup.cs to use the dbcontext.

---
+++ AspNetCoreWithGraphQL/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreWithGraphQL.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWithGraphQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Product> Products { get; set; }
+    }
+}
